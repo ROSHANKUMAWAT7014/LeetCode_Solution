16447ee893efd305de2e5765c88406f9584a506d
Roshan Kumar <207435851+ROSHANKUMAWAT7014@users.noreply.github.com>
2026-08-06 16:32:58 +0530

    Time: 3 ms (100.00%) | Memory: 175.5 MB (57.67%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int[] rearrangeArray(int[] arr) {
-        int n=arr.length;
-        ArrayList<Integer> a= new ArrayList<>();
-        ArrayList<Integer> b= new ArrayList<>();
-        for(int i =0;i<n;i++){
-            if(arr[i]<0){
-                a.add(arr[i]);
+    public int[] rearrangeArray(int[] nums) 
+    {
+        int l = nums.length;
+        int ans[] = new int[l];
+        int posIndex = 0 , negIndex = 1;
+        for (int i=0;i<l;i++)
+        {
+            if(nums[i] >= 0)
+            {
+                ans[posIndex] = nums[i];
+                posIndex = posIndex + 2;
             }
-            else{
-                b.add(arr[i]);
+            else if (nums[i] < 0)
+            {
+                ans[negIndex] = nums[i];
+                negIndex = negIndex + 2;
             }
-        }
-        ArrayList<Integer> c= new ArrayList<>();
-        for(int i=0;i<n/2;i++){
-            c.add(b.get(i));
-            c.add(a.get(i));
-        }
-        int[] num =new int[n];
-        for(int i=0;i<n;i++){
-            num[i] = c.get(i);
-        }
-        return num;
+        } 
+        return ans;
     }
 }

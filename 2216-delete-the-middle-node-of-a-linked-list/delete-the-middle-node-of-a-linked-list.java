7019b813de247a885b807d53e7ad5a078fca2b9d
Roshan Kumar <207435851+ROSHANKUMAWAT7014@users.noreply.github.com>
2026-08-24 17:05:58 +0530

    Time: 3 ms (99.93%) | Memory: 202.7 MB (42.50%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,16 +10,29 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head.next==null){
-            return null;
+        if(head.next == null) return null;
+        ListNode fast = head.next.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        ListNode slow=null;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=(slow == null)? fast:slow.next;
-            fast=fast.next.next;
-        }
-        slow.next=slow.next.next;
+        slow.next = slow.next.next;
         return head;
     }
 }
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         if(head.next==null){
+//             return null;
+//         }
+//         ListNode slow=null;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null){
+//             slow=(slow == null)? fast:slow.next;
+//             fast=fast.next.next;
+//         }
+//         slow.next=slow.next.next;
+//         return head;
+//     }
+// }

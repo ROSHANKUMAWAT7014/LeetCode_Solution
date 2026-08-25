62eda5138c79cefb8651c45c397f7494a7f7896d
Roshan Kumar <207435851+ROSHANKUMAWAT7014@users.noreply.github.com>
2026-08-25 16:21:39 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (19.63%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,40 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp1=l1;
-        ListNode temp2=l2;
-        ListNode ans=new ListNode();
-        ListNode result =ans;
-        int b=0;
+        ListNode temp1 = l1, temp2 = l2;
+        ListNode dummy = new ListNode(0);
+        ListNode ans = dummy;
+        int carry = 0;
 
-        while(temp1!=null || temp2!=null || b!=0){
-            int val1=(temp1!=null)? temp1.val:0;
-            int val2=(temp2!=null)? temp2.val:0;
-            int a=val1+val2+b;
-            if(a<10){
-                ListNode node1= new ListNode(a);
-                ans.next=node1;
-                ans=ans.next;
-                b=0;
-            }
-            else{
-                ListNode node2= new ListNode(a-10);
-                ans.next=node2;
-                b=1;
-                ans=ans.next;
-            }
+        while (temp1 != null || temp2 != null || carry != 0) {
+            int val1 = (temp1 != null) ? temp1.val : 0;
+            int val2 = (temp2 != null) ? temp2.val : 0;
+            
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            
+            ans.next = new ListNode(sum % 10);    
+            ans = ans.next;
+
             if (temp1 != null) temp1 = temp1.next;
             if (temp2 != null) temp2 = temp2.next;
         }
-    return result.next;
+
+        return dummy.next;
     }
 }

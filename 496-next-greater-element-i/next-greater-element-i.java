92943627ee3411b80791dcda46738ffd6fcfca7c
Roshan Kumar <207435851+ROSHANKUMAWAT7014@users.noreply.github.com>
2026-08-29 23:12:07 +0530

    Time: 2 ms (99.51%) | Memory: 45.8 MB (10.76%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int[] ans = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int target = nums1[i];
-            int nextGreater = -1;
-
-            // Step 1: Find the index of target in nums2
-            int j = 0;
-            while (j < m && nums2[j] != target) {
-                j++;
+        Stack<Integer> st= new Stack<>();
+        int n=nums1.length;
+        int m=nums2.length;
+        int[] ans=new int[10001];
+        for(int i=m-1;i>=0;i--){
+            int num=nums2[i];
+            while(!st.isEmpty() && st.peek()<=num){
+                st.pop();
             }
-
-            // Step 2: Search to the right of target in nums2 for the first greater element
-            for (int k = j + 1; k < m; k++) {
-                if (nums2[k] > target) {
-                    nextGreater = nums2[k];
-                    break;
-                }
-            }
-
-            ans[i] = nextGreater;
+            ans[num]=st.isEmpty()?-1:st.peek();
+            st.push(num);
         }
-
-        return ans;
+        for(int i=0;i<n;i++){
+            nums1[i]=ans[nums1[i]];
+        }
+        return nums1;
     }
 }
